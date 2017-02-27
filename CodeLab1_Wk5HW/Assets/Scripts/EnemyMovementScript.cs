bf30ec5d96b55d9c2cc03cc2b1bb6e9d83f266f1
DavidFrisch <dmf463@nyu.edu>
2017-02-26 22:34:07 -0500

    added in some player controls to set up the scene

---
+++ CodeLab1_Wk5HW/Assets/Scripts/EnemyMovementScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementScript : MonoBehaviour
+{
+
+    public float speed;
+    GameObject deer;
+    PlayerControlScript playerControlScript;
+
+    // Use this for initialization
+    void Start()
+    {
+        deer = GameObject.Find("Player");
+        playerControlScript = deer.GetComponent<PlayerControlScript>();
+    }
+
+    void OnCollisionEnter (Collision other)
+    {
+        if (other.gameObject.tag == "Beginning" || other.gameObject.tag == "End")
+        {
+            speed *= -1;
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        hunterMove(Vector3.left, speed);
+
+        //Debug.Log("Distance between Hunter and Deer = " + Vector3.Distance(transform.position, deer.transform.position));
+
+        if(Vector3.Distance(transform.position, deer.transform.position) < 5)
+        {
+            Debug.Log("I SEE YOU");
+            speed = 0;
+            playerControlScript.enabled = false;
+        }
+    }
+
+    void hunterMove(Vector3 dir, float movementSpeed)
+    {
+        transform.Translate(dir * movementSpeed * Time.deltaTime);
+    }
+
+}
---
+++ CodeLab1_Wk5HW/Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour {
+
+	public float walkSpeed = 5;
+    public float runSpeed = 10;
+
+	public KeyCode upKey = KeyCode.W;
+	public KeyCode downKey = KeyCode.S;
+	public KeyCode leftKey = KeyCode.A;
+	public KeyCode rightKey = KeyCode.D;
+    public KeyCode shiftKey = KeyCode.RightShift;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Move(Vector3.up, upKey);
+        //Move(Vector3.down, downKey);
+
+        if (Input.GetKey(shiftKey))
+        {
+            Move(Vector3.left, leftKey, runSpeed);
+            Move(Vector3.right, rightKey, runSpeed);
+        }
+        else
+        {
+            Move(Vector3.left, leftKey, walkSpeed);
+            Move(Vector3.right, rightKey, walkSpeed);
+        }
+
+
+//		if(Input.GetKey(KeyCode.W)){
+////			Debug.Log("This is working");
+//			transform.Translate(Vector3.up * speed * Time.deltaTime);
+//		}
+//
+//		if(Input.GetKey(KeyCode.S)){
+//			transform.Translate(Vector3.down * speed * Time.deltaTime);
+//		}
+	}
+
+	void Move(Vector3 dir, KeyCode key, float movementSpeed){
+		if(Input.GetKey(key)){
+			transform.Translate(dir * movementSpeed * Time.deltaTime);
+		}
+
+	}
+}
